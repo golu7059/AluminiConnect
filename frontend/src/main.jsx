1ee9a97482dcafdd61b49d85f76f8a6e9742405f
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-23 01:50:04 +0530

    added scroll viewer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/sore.js";
 
-createRoot(document.getElementById('root')).render(
+import ScrollRevealProvider from "./utils/ScrollRevealProvider.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ScrollRevealProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ScrollRevealProvider>
+    </Provider>
+  </StrictMode>
+);
